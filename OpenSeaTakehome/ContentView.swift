733ed8564cb3c59afd16776b675835510868464f
jordan-romeroporter <jordan.romeroporter@gmail.com>
2025-09-24 10:14:52 -0700

    Initial Commit

---
+++ OpenSeaTakehome/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OpenSeaTakehome
+//
+//  Created by Jordan Romero Porter on 9/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OpenSeaTakehomeTests/OpenSeaTakehomeTests.swift
@@ -0,0 +1,16 @@
+//
+//  OpenSeaTakehomeTests.swift
+//  OpenSeaTakehomeTests
+//
+//  Created by Jordan Romero Porter on 9/24/25.
+//
+
+import Testing
+
+struct OpenSeaTakehomeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
